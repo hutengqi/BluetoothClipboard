7bce0d492735ab12b5fba2ae4b9fceb598dda18b
Ht7sOusuLocal <18838292213@163.com>
2024-11-18 17:19:19 +0800

    feat：task and device list view

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Scighost.WinUILib.Helpers;
+using Windows.Graphics;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,14 +17,29 @@ namespace BluetoothClipboard
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private IntPtr windowHandle;
+
+        private AppWindow appWindow;
+
+        private SystemBackdrop systemBackdrop;
+
         public MainWindow()
         {
             this.InitializeComponent();
+
+            windowHandle = WindowNative.GetWindowHandle(this);
+            WindowId id = Win32Interop.GetWindowIdFromWindow(windowHandle);
+            appWindow = AppWindow.GetFromWindowId(id);
+
+            appWindow.Resize(new SizeInt32(450,800));
+
+            systemBackdrop = new SystemBackdrop(this);
+            systemBackdrop.TrySetMica(fallbackToAcrylic: true);
         }
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            bluetoothDeviceList.Visibility = Visibility.Visible ;
         }
     }
 }
